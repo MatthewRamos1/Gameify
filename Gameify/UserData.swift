fb63c7f3459bdce443593278acad469d4f0dc360
Matthew Ramos <mmramos309@gmail.com>
2020-03-08 14:33:44 -0400

    Initial Commit

---
+++ Gameify/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 3/8/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var strength = 0
+    var constitution = 0
+    var intelligence = 0
+    var wisdom = 0
+    var dexAgi = 0
+    var charisma = 0
+}
---
+++ Gameify/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 3/8/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playerImage: UIImageView!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var strengthLabel: UILabel!
+    @IBOutlet weak var constitutionLabel: UILabel!
+    @IBOutlet weak var intelligenceLabel: UILabel!
+    @IBOutlet weak var wisdomLabel: UILabel!
+    @IBOutlet weak var dexAgiLabel: UILabel!
+    @IBOutlet weak var charismaLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
