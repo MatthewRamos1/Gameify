15d5431ab70800903a790adaebc96b9724e39628
Matthew Ramos <mmramos309@gmail.com>
2020-07-26 18:07:16 -0400

    Changed collection view to table view, created new group for controllers

--- Gameify/DatabaseServices.swift
+++ Gameify/DatabaseServices.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class DatabaseServices {
+    
+    static let userCollection = "users"
+    
+    private let db = Firestore.firestore()
+    
+    static let shared = DatabaseServices()
+    
+    public func createDatabaseUser(authDataResult: AuthDataResult, completion: @escaping (Result<Bool, Error>) -> ()) {
+        
+        let user = authDataResult.user
+        
+        db.collection(DatabaseServices.userCollection).document(authDataResult.user.uid).setData(["email" : user.email ?? "", "userId": user.uid, "username": user.displayName ?? ""]) { (error) in
+            
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(true))
+            }
+        }
+    }
+}
