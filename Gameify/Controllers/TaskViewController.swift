41fa2d1a166510f7591c128a00378dc723b1f408
Matthew Ramos <mmramos309@gmail.com>
2020-07-26 21:03:38 -0400

    Set up tableview data source, initialized dummy task

--- Gameify/Controllers/TaskViewController.swift
+++ Gameify/Controllers/TaskViewController.swift
@@ -12,10 +12,26 @@ class TaskViewController: UIViewController {
     
     @IBOutlet weak var taskTV: UITableView!
     
+    var tasks = [Task(title: "Work on Passion Project", description: "Duration: 1 Hour", rating: 4, statUps: [.constitution])]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskTV.dataSource = self
 
     }
     
 
+}
+
+extension TaskViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "someCell", for: indexPath)
+        return cell
+    }
+    
+    
 }
