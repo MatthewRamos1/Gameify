a0111650445bd1250b7c1ea0ecc83e4270700b2a
Matthew Ramos <mmramos309@gmail.com>
2020-08-04 18:23:54 -0400

    Rating system implemented into task creation, changes stars in UI to reflect difficulty of task, clicking on star buttons allows for toggling of different ratings

--- Gameify/Controllers/CreateTaskViewController.swift
+++ Gameify/Controllers/CreateTaskViewController.swift
@@ -9,22 +9,94 @@
 import UIKit
 import FirebaseAuth
 class CreateTaskViewController: UIViewController {
-
+    
     @IBOutlet weak var taskImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var repeatRepSegmented: UISegmentedControl!
+    @IBOutlet weak var oneStar: UIButton!
+    @IBOutlet weak var twoStar: UIButton!
+    @IBOutlet weak var threeStar: UIButton!
+    @IBOutlet weak var fourStar: UIButton!
+    @IBOutlet weak var fiveStar: UIButton!
+    
+    var rating = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    @IBAction func starButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            if rating == 2 {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.lefthalf.fill"), for: .normal)
+                rating = 1
+            } else {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                twoStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                threeStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                fourStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                fiveStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                rating = 2
+            }
+        case 1:
+            if rating == 4 {
+                twoStar.setBackgroundImage(UIImage(systemName: "star.lefthalf.fill"), for: .normal)
+                rating = 3
+            } else {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                twoStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                threeStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                fourStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                fiveStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                rating = 4
+            }
+        case 2:
+            if rating == 6 {
+                threeStar.setBackgroundImage(UIImage(systemName: "star.lefthalf.fill"), for: .normal)
+                rating = 5
+            } else {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                twoStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                threeStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                fourStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                fiveStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                rating = 6
+            }
+        case 3:
+            if rating == 8 {
+                fourStar.setBackgroundImage(UIImage(systemName: "star.lefthalf.fill"), for: .normal)
+                rating = 7
+            } else {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                twoStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                threeStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                fourStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                fiveStar.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+                rating = 8
+            }
+        default:
+            if rating == 10 {
+                fiveStar.setBackgroundImage(UIImage(systemName: "star.lefthalf.fill"), for: .normal)
+                rating = 9
+            } else {
+                oneStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                twoStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                threeStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                fourStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                fiveStar.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+                rating = 10
+            }
+        }
     }
     
     @IBAction func createTaskPressed(_ sender: UIButton) {
         guard let title = titleTextField.text, let description = descriptionTextView.text, let uid = Auth.auth().currentUser?.uid else {
             return
         }
-        let task = Task(title: title, description: description, rating: 0, statUps: [.strength], repeatable: .always, id: UUID().uuidString)
+        let task = Task(title: title, description: description, rating: rating, statUps: [.strength], repeatable: .always, id: UUID().uuidString)
         DatabaseServices.shared.createUserTask(uid: uid, task: task) { [weak self] (result) in
             switch result {
             case .failure(let error):
