e2c1506916ab143c377e99c02703551f7e2d3ed9
Matthew Ramos <mmramos309@gmail.com>
2020-08-17 19:27:32 -0400

    Created new vcs for game/combat elements, began editing cell for list of levels

---
+++ Gameify/Controllers/DungeonProgressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DungeonProgressViewController.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/17/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class DungeonProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Gameify/Models/DungeonData.swift
+++ Gameify/Models/DungeonData.swift
@@ -9,11 +9,14 @@
 import Foundation
 
 struct Dungeon {
+    let name: String
     let enemies: [Enemy]
+    let cellBackground: String
     let background: String
+    let index: Int
 }
 
 struct DungeonList {
-    let dungeon1 = Dungeon(enemies: dungeon1Enemies, background: "")
+    let dungeon1 = Dungeon(name: "Dungeon 1", enemies: dungeon1Enemies, cellBackground: "", background: "", index: 0)
     static let dungeon1Enemies = [Enemy(name: "Slime", level: 1, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, goldDrop: 1, itemDrops: nil)]
 }
