d7ce1440d5d58cb65db97468163ed8adfdfbe401
Matthew Ramos <mmramos309@gmail.com>
2020-08-17 17:36:43 -0400

    Created models for enemies and items

---
+++ Gameify/Models/EnemyData.swift
@@ -0,0 +1,21 @@
+//
+//  EnemyData.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/17/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct Enemy {
+    var level: Int
+    var strength: Int
+    var constitution: Int
+    var intelligence: Int
+    var wisdom: Int
+    var dexAgi: Int
+    var charisma: Int
+    var goldDrop: Int
+    var itemDrops: [Item]
+}
---
+++ Gameify/Models/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/17/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    
+}
