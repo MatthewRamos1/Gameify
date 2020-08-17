d13aa1468d558dca4cd58253907e5e5f86c5a6b6
Matthew Ramos <mmramos309@gmail.com>
2020-08-17 19:34:18 -0400

    Changed dungeon model to class, set up initializer

--- Gameify/Models/DungeonData.swift
+++ Gameify/Models/DungeonData.swift
@@ -8,15 +8,25 @@
 
 import Foundation
 
-struct Dungeon {
+class Dungeon {
     let name: String
     let enemies: [Enemy]
     let cellBackground: String
     let background: String
     let index: Int
+    let remainingEnemies: Int
+    
+    init(name: String, enemies: [Enemy], cellBackground: String, background: String, index: Int, remainingEnemies: Int) {
+        self.name = name
+        self.enemies = enemies
+        self.cellBackground = cellBackground
+        self.background = background
+        self.index = index
+        self.remainingEnemies = remainingEnemies
+    }
 }
 
 struct DungeonList {
-    let dungeon1 = Dungeon(name: "Dungeon 1", enemies: dungeon1Enemies, cellBackground: "", background: "", index: 0)
+    let dungeon1 = Dungeon(name: "Dungeon 1", enemies: dungeon1Enemies, cellBackground: "", background: "", index: 0, remainingEnemies: 6)
     static let dungeon1Enemies = [Enemy(name: "Slime", level: 1, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, goldDrop: 1, itemDrops: nil)]
 }
