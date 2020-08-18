87fc26fab1e91ed41673c72e669b0122bc0c11db
Matthew Ramos <mmramos309@gmail.com>
2020-08-18 03:05:15 -0400

    Created class for storage services

---
+++ Gameify/StorageServices.swift
@@ -0,0 +1,15 @@
+//
+//  StorageServices.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/18/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import Foundation
+import FirebaseStorage
+
+class StorageServices {
+    
+}
+
