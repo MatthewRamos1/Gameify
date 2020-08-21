df4ee3c269b493375e032a67c435f2cded5167aa
Matthew Ramos <mmramos309@gmail.com>
2020-08-20 21:21:52 -0400

    Created damage label for view control that is animated between alpha 1 and then back to 0 when damage is calculated

--- Gameify/Controllers/CombatViewController.swift
+++ Gameify/Controllers/CombatViewController.swift
@@ -13,6 +13,7 @@ class CombatViewController: UIViewController {
     @IBOutlet weak var backgroundIV: UIImageView!
     @IBOutlet weak var enemyIV: UIImageView!
     @IBOutlet weak var enemyHealthProgress: UIProgressView!
+    @IBOutlet weak var damageLabel: UILabel!
     
     var dungeon: Dungeon?
     var enemy: Enemy!
@@ -22,6 +23,8 @@ class CombatViewController: UIViewController {
         super.viewDidLoad()
         fetchUser()
         enemyHealthProgress.progress = 1.0
+        damageLabel.alpha = 0.0
+        damageLabel.textColor = .red
     }
     
     private func fetchUser() {
@@ -42,6 +45,7 @@ class CombatViewController: UIViewController {
             return
         }
         let damage = CombatFormulas.damageCalculation(strength: playerStats.strength, constitution: enemy.constitution, weaponAttack: nil, armorDefense: nil)
+        damageLabel.text = String(damage)
         enemy.currentHealth = enemy.currentHealth - damage
         let healthFloat = (Float(enemy.currentHealth)) / (Float(enemy.maxHealth))
         print(damage)
@@ -52,6 +56,14 @@ class CombatViewController: UIViewController {
                     self.enemyHealthProgress.alpha = 0
                 }
             }, completion: nil)
+        UIView.animate(withDuration: 0.4, delay: 0.2 , animations: {
+            self.damageLabel.alpha = 1
+        }) { completion in
+            UIView.animate(withDuration: 0.4, delay: 0.2 , animations: {
+                self.damageLabel.alpha = 0
+            })
+        }
+        
     }
     
 }
