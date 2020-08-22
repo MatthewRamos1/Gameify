e16e445e01c0a9524ce2e26f2d403f0f5f3f272e
Matthew Ramos <mmramos309@gmail.com>
2020-08-21 22:02:05 -0400

    Set up get enemy function with random chance depending on ranges as probabilities

--- Gameify/Models/DungeonData.swift
+++ Gameify/Models/DungeonData.swift
@@ -31,11 +31,20 @@ struct DungeonList {
     static let dungeon1 = Dungeon(name: "Dungeon 1", enemies: dungeon1Enemies, cellBackground: "", background: "", index: 0, remainingEnemies: 6)
     static let dungeon2 = Dungeon(name: "Dungeon 2", enemies: dungeon2Enemies, cellBackground: "", background: "", index: 0, remainingEnemies: 8)
     
-    static let dungeon1Enemies = [Enemy(name: "Slime", level: 1, currentHealth: 10, maxHealth: 10, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, encounterChance: 50, goldDrop: 1, itemDrops: nil), Enemy(name: "Goblin", level: 2, currentHealth: 12, maxHealth: 12, strength: 2, constitution: 2, intelligence: 1, wisdom: 0, dexAgi: 4, charisma: 3, encounterChance: 50, goldDrop: 1, itemDrops: nil)]
-    static let dungeon2Enemies = [Enemy(name: "Slime2", level: 1, currentHealth: 10, maxHealth: 10, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, encounterChance: 50, goldDrop: 1, itemDrops: nil)]
+    static let dungeon1Enemies = [Enemy(name: "Slime", level: 1, currentHealth: 10, maxHealth: 10, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, encounterRange: 0...50, goldDrop: 1, itemDrops: nil), Enemy(name: "Goblin", level: 2, currentHealth: 12, maxHealth: 12, strength: 2, constitution: 2, intelligence: 1, wisdom: 0, dexAgi: 4, charisma: 3, encounterRange: 51...100, goldDrop: 1, itemDrops: nil)]
+    static let dungeon2Enemies = [Enemy(name: "Slime2", level: 1, currentHealth: 10, maxHealth: 10, strength: 1, constitution: 1, intelligence: 1, wisdom: 1, dexAgi: 1, charisma: 1, encounterRange: 0...50, goldDrop: 1, itemDrops: nil)]
     static let dungeonList = [dungeon1, dungeon2]
     
     static public func getEnemy(dungeon: Dungeon) -> Enemy {
-        dungeon.enemies.first!
+        let enemies = dungeon.enemies
+        let randomVal = Int.random(in: 0...100)
+        for enemy in enemies {
+            if enemy.encounterRange.contains(randomVal) {
+                return enemy
+            } else {
+                return enemies.first!
+            }
     }
+        return enemies.first!
+}
 }
--- Gameify/Models/EnemyData.swift
+++ Gameify/Models/EnemyData.swift
@@ -19,11 +19,11 @@ class Enemy {
     var wisdom: Int
     var dexAgi: Int
     var charisma: Int
-    var encounterChance: Int
+    var encounterRange: ClosedRange<Int>
     var goldDrop: Int
     var itemDrops: [Item]?
     
-    init(name: String, level: Int, currentHealth: Int, maxHealth: Int, strength: Int, constitution: Int, intelligence: Int, wisdom: Int, dexAgi: Int, charisma: Int, encounterChance: Int, goldDrop: Int, itemDrops: [Item]?) {
+    init(name: String, level: Int, currentHealth: Int, maxHealth: Int, strength: Int, constitution: Int, intelligence: Int, wisdom: Int, dexAgi: Int, charisma: Int, encounterRange: ClosedRange<Int>, goldDrop: Int, itemDrops: [Item]?) {
         self.name = name
         self.level = level
         self.currentHealth = currentHealth
@@ -34,7 +34,7 @@ class Enemy {
         self.wisdom = wisdom
         self.dexAgi = dexAgi
         self.charisma = charisma
-        self.encounterChance = encounterChance
+        self.encounterRange = encounterRange
         self.goldDrop = goldDrop
         self.itemDrops = itemDrops
     }
