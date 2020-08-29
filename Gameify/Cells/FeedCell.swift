d59cc10bbcf659b948ddea89ca0aeeb94fb4ed62
Matthew Ramos <mmramos309@gmail.com>
2020-08-28 23:52:20 -0400

    Created doc for feed cell

---
+++ Gameify/Cells/FeedCell.swift
@@ -0,0 +1,19 @@
+//
+//  FeedCell.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/28/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var feedFriendImageView: UIImageView!
+    @IBOutlet weak var feedFriendNameLabel: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
+}
