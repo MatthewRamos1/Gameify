b10e586f88cb6b7d827af6e58477acfe9e1dd8e3
Matthew Ramos <mmramos309@gmail.com>
2020-08-30 19:06:46 -0400

    Placeholder items created

--- Gameify/Models/Equipment.swift
+++ Gameify/Models/Equipment.swift
@@ -25,13 +25,20 @@ struct Equipment {
     
     let name: String
     let description: String
+    let id: Int
     let strBonus: Int?
     let conBonus: Int?
     let intBonus: Int?
     let wisBonus: Int?
     let dexBonus: Int?
     let chaBonus: Int?
+    let sellValue: Int
     let equipmentType: EquipmentType
     let rarity: Rarity
-    
+}
+
+struct EquipmentList {
+    let item0 =  Equipment(name: "Jelly Sword", description: "", id: 0, strBonus: 0, conBonus: nil, intBonus: nil, wisBonus: nil, dexBonus: nil, chaBonus: nil, sellValue: 1, equipmentType: .weapon, rarity: .common)
+    let item1 = Equipment(name: "Gas-Powered Stick", description: "", id: 1, strBonus: 1, conBonus: nil, intBonus: nil, wisBonus: nil, dexBonus: nil, chaBonus: nil, sellValue: 1, equipmentType: .weapon, rarity: .common)
+    let item2 = Equipment(name: "Goblin Shiv", description: "", id: 2, strBonus: 2, conBonus: nil, intBonus: nil, wisBonus: nil, dexBonus: 1, chaBonus: nil, sellValue: 3, equipmentType: .weapon, rarity: .uncommon)
 }
