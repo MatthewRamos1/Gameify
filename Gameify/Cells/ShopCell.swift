313d8159bc49f00b65175d3d6d74942f3accfff0
Matthew Ramos <mmramos309@gmail.com>
2020-08-30 23:38:56 -0400

    Customized ui for shop cell, created empty cocoa touch for said cell

---
+++ Gameify/Cells/ShopCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShopCell.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 8/30/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ShopCell: UITableViewCell {
+
+
+}
