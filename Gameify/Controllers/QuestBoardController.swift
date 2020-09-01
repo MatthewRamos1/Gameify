2423943a504f3e80714bb016f8303c8f94901d10
Matthew Ramos <mmramos309@gmail.com>
2020-09-01 00:56:11 -0400

    Created QuestBoardController, constrained items within

---
+++ Gameify/Controllers/QuestBoardController.swift
@@ -0,0 +1,20 @@
+//
+//  QuestBoardController.swift
+//  Gameify
+//
+//  Created by Matthew Ramos on 9/1/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class QuestBoardController: UIViewController {
+
+    @IBOutlet weak var questBoardCollectionView: UICollectionView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
