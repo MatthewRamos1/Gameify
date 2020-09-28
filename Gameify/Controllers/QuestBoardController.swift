34bd92009c7e7076cd5572206bfe795a0d9a7ebb
Matthew Ramos <mmramos309@gmail.com>
2020-09-28 00:30:04 -0400

    Set up listener on quest board view controller

--- Gameify/Controllers/QuestBoardController.swift
+++ Gameify/Controllers/QuestBoardController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 class QuestBoardController: UIViewController {
 
     @IBOutlet weak var questBoardCollectionView: UICollectionView!
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    var tasksListener: ListenerRegistration?
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupTasksListener()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        tasksListener?.remove()
+    }
+    
+    private func setupTasksListener() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        tasksListener = Firestore.firestore().collection(DatabaseServices.userCollection).document(uid).collection(DatabaseServices.taskCollection).addSnapshotListener( { (snapshot, error) in
+            if let error = error {
+                self.showAlert(title: "Error", message: error.localizedDescription)
+            } else if let snapshot = snapshot {
+                let tempTasks =  snapshot.documents.map { Task($0.data())}
+                self.tasks = tempTasks
+                
+            }
+        })
     }
 }
